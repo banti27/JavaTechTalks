5cd07fd732037a0d0abcb9c7796df28b79b16def
Bal Vikash Sharma <balvikash.sharma@wissen.com>
2024-02-19 01:14:54 +0530

    Implemented Spotless

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  public void print(String value) {
+    System.out.println("FROM STRING");
+  }
+
+  public void print(Object value) {
+    System.out.println("FROM OBJECT");
+  }
 }
