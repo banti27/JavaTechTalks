80ed2e38e73fde3f65ec5d7d75c8fe2804b74019
Bal Vikash Sharma <balvikash.sharma@wissen.com>
2024-03-18 12:03:14 +0530

    Added comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,21 +23,34 @@ repositories {
 }
 
 dependencies {
+    // Handle HTTP request
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // Process database queries
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
 
+    // Help in removing bolier plate code gettters, setters, hashcode, equals etc.
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
+    // Test library for sring boot
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    // For documentation Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("openApiVersion")}")
+
+    // Entity metamodel generation
     annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:${property("hibernateJpaMetaModelVersion")}")
+
+    // Process csv files
     implementation("com.opencsv:opencsv:${property("openCsvVersion")}")
+
+    // Java bean mapping
     implementation ("org.mapstruct:mapstruct:${property("mapstructVersion")}")
     annotationProcessor ("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
+
+    // Common utility library
     implementation("org.apache.commons:commons-lang3:${property("apacheCommonsLang3")}")
 
 
